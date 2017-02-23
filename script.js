5d7340e0d08fb2a6c7713b2cb11f295b36bd9a91
Devin Burke <devburke@gmail.com>
2017-02-23 03:16:28 +0000

    Got a lot done. Added pencil, eraser, and rainbow tools, and changed lots of structure and organization around likewise. Also added things like validating user input. It should be fully functional and complete, all it needs now is some new styling.

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 function drawGrid(squaresPerSide) {
+    
     //Add rows
     for (var i = 0; i < squaresPerSide; i++) {
         $('#sketchpad').append('<div class="row"></div>');
     }
-
+    
     //Fill rows
     for (var i = 0; i < squaresPerSide; i++) {
         $('.row').append('<div class="square"></div>');
     }
-
+    
     //Set square sizes
     var squareDimension = $('#sketchpad').width() / squaresPerSide;
     $('.square').css({
@@ -18,20 +19,72 @@ function drawGrid(squaresPerSide) {
 }
 
 $(document).ready(function() {
-    //Set it up
-    drawGrid(16);
-
+   
+   //Set up
+    var squaresPerSide = 16;
+    drawGrid(squaresPerSide);
+    var mode = 'pen';
+    
     //Draw!
     $('#sketchpad').on('mouseenter', '.square', function() {
-        $(this).addClass('drawn');
+        var opacity = +$(this).css('opacity');
+        switch (mode) {
+            case 'pen':
+                $(this).css({'opacity': 1});
+                break;
+            case 'pencil':
+                if (opacity + .1 < 1) {
+                    $(this).css({'opacity': opacity + .1});
+                } else {
+                    $(this).css({'opacity': 1});
+                }
+                break;
+            case 'eraser':
+                if (opacity > 0) {
+                    $(this).css({'opacity': opacity - .2});
+                }
+                
+                //Double check for browser weirdness like Safari not wanting to set it all the way to 0, and adjust for erasing too far past 0, since it erases faster than the pencil draws
+                var newOpacity = $(this).css('opacity')
+                if (opacity - newOpacity < .015 || newOpacity < 0) {
+                    $(this).css({'opacity': 0});
+                }
+                break;
+            case 'rainbow':
+                var r = Math.floor(Math.random() * 256);
+                var g = Math.floor(Math.random() * 256);
+                var b = Math.floor(Math.random() * 256);
+                $(this).css({'opacity': 1, 'background-color': 'rgb(' + r + ',' + g + ',' + b + ')'});
+                break;
+        }
     });
 
     //Reset
     $('#reset').on('click', function() {
-        $('#sketchpad').empty();
 
-        //Add something here to verify that input was an integer
-        var squaresPerSide = prompt('How many squares per side would you like?',16);
-        drawGrid(squaresPerSide);
+        //Get and validate user input
+        var newSquaresPerSide;
+        var invalidInput;
+        do {
+            newSquaresPerSide = prompt('How many squares per side would you like?', squaresPerSide);
+            var isNull = newSquaresPerSide === null;
+            var isInteger = $.isNumeric(newSquaresPerSide) && Number.isInteger(+newSquaresPerSide);
+            var isPositive = newSquaresPerSide > 0;
+            invalidInput = !isNull && !(isInteger && isPositive);
+            if (invalidInput) {
+                alert('Only positive integers (1, 2, 3, etc.)');
+            }
+        } while (invalidInput);
+        
+        if (newSquaresPerSide !== null) {
+            squaresPerSide = newSquaresPerSide;
+            $('#sketchpad').empty();
+            drawGrid(squaresPerSide);
+        }
+    });
+
+    //Change mode
+    $('#modes').on('click','button',function(){
+        mode = $(this).attr('id');
     });
 });
